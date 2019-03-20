3474dd450e443341c7c85adb26d9abf1dc6e5b57
learningNicolas <q641259875@outlook.com>
2019-03-20 20:55:38 +0800

    success start server

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+#加载静态界面index首页s
+def index(request):
+    # request.META["CSRF_COOKIE_USED"] = True
+    return render(request,'index.html')
